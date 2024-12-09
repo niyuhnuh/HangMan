270fe6e29f2b614dda3893becca1ec936cb8275f
Niana Tajanlangit <ntajanlangit@svi.com.ph>
2024-12-09 16:23:12 +0800

    Add 'Words' class and pseudocode for game

--- HangMan/src/com/projects/main/Main.java
+++ HangMan/src/com/projects/main/Main.java
@@ -1,5 +1,9 @@
 package com.projects.main;
 
 public class Main {
-
+	public static void main(String[] args) {
+		//Initialize words for the game from input txt file
+		//Ask the user for the input difficulty of the game
+		//Play the game		
+	}
 }
